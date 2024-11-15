564b971a58f203d0552e0a62c348a2423f8f5705
Lex <vasickin@mail.ru>
2024-11-15 12:26:47 +0300

    finished working with Serislizable task2

--- Serializable.java
+++ Serializable.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+
 public interface Serializable {
-    
+    void saveToFile(String filename, Object obj) throws IOException;
+    Object loadFromFile(String filename) throws IOException, ClassNotFoundException;
 }
